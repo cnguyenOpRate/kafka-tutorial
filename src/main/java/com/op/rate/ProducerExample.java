e2af973bc38aff8b7c048391649e0ecd830af76f
Christian Nguyen Van Than <cnguyen@op-rate.com>
2019-01-17 11:56:36 +0100

    implementation example for exercise 1

--- src/main/java/com/op/rate/ProducerExample.java
+++ src/main/java/com/op/rate/ProducerExample.java
@@ -15,13 +15,21 @@ public class ProducerExample {
 
     void sendMessage() {
         Properties props = new Properties();
-        // TODO fill properties
+
+        props.put("bootstrap.servers", "broker:9092");
+        props.put("acks", "1");
+        props.put("delivery.timeout.ms", 30000);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         Producer<String, String> producer = new KafkaProducer<>(props);
 
         for (int i = 0; i < 100; i++) {
-            // TODO replace the parameters to send a message
-            producer.send(new ProducerRecord<>(null, null, null));
+            // simply send a number as message key and payload
+            producer.send(new ProducerRecord<>("test-topic", Integer.toString(i), Integer.toString(i)));
         }
 
         try {
